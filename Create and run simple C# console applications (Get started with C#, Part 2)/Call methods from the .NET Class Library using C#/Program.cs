484ef202e8e84f96e8c22a0ed5b71c70f13bff90
Angel Raychev <angelraychev@gmail.com>
2025-01-31 17:05:30 +0200

    Create and run simple C# console applications traning started

---
+++ Create and run simple C# console applications (Get started with C#, Part 2)/Call methods from the .NET Class Library using C#/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//Random dice = new Random();
+//int num = dice.Next();
+//Console.WriteLine(num);
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
+
+
